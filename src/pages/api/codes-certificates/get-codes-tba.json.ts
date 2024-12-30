76a2a4e242ca6189696f82c1ee05dc6569773fd7
Carlos Morales <cmja.27@gmail.com>
2024-12-30 12:36:18 -0400

    feature: BUsqueda de certificados de entrenadores tba

--- src/pages/api/codes-certificates/get-codes-tba.json.ts
+++ src/pages/api/codes-certificates/get-codes-tba.json.ts
@@ -1,10 +1,11 @@
 import type { APIRoute } from "astro"
-import { db, Certificate, Codes, eq, isNull, and } from "astro:db";
+import { db, Certificate, Codes, eq, isNull, and, or } from "astro:db";
 import { not } from "drizzle-orm";
 
 export const GET: APIRoute = async ({ params, request }) => {
     const searchParam = new URL(request.url).searchParams.get("search");
     const idCertificarteTBA = 14;
+    const idTraningCertificarteTBA = 20;
     return new Response(JSON.stringify({
         message: "Consuta realiza con éxito!",
         data: await db.select()
@@ -12,7 +13,10 @@ export const GET: APIRoute = async ({ params, request }) => {
         .where(
             and(
                 isNull(Codes.deleted_at),
-                eq(Codes.certificate_id, idCertificarteTBA),
+                or(
+                    eq(Codes.certificate_id, idCertificarteTBA),
+                    eq(Codes.certificate_id, idTraningCertificarteTBA)
+                ),
                 eq(Codes.name, searchParam)
             )
         )
